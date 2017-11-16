2ab5d6fbb3e018ed4c12fbc09a060db65c5b8c07
Bastian Müller <bastian@turbolent.com>
2017-11-15 21:03:55 -0800

    sort and deduplicate

--- data/fetch-unique-identifier-properties.sh
+++ data/fetch-unique-identifier-properties.sh
@@ -3,4 +3,7 @@
 rsparql --results=csv --query=unique-identifier-properties.sparql --service="https://query.wikidata.org/sparql" \
 	| tail -n +2 \
 	| grep 'http://www.wikidata.org/entity/P' \
-	| sed -e 's|http://www.wikidata.org/entity/P|http://www.wikidata.org/prop/direct/P|'
+	| sed -e 's|http://www.wikidata.org/entity/P|http://www.wikidata.org/prop/direct/P|' \
+	| sort \
+	| uniq
+
