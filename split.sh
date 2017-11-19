4acd14db5946553286cd8b8b89b7932706c81b6c
Bastian Müller <bastian@turbolent.com>
2017-11-19 14:16:32 -0800

    add split script

---
+++ split.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# alternatively:
+# zcat $1 | parallel --block 2M -j4 --pipe --round-robin "gzip > $2/part_{#}.gz"
+
+zcat $1 | split  -l20000000 -a 3 -d --filter='gzip > $FILE.gz' - $2/part_
